333ff397d6d1b6ed62b1f3975669d18d75003307
Ariel Lenis <ariel.lenisd@gmail.com>
2017-02-24 21:55:21 -0400

    Additional modifications to build.sh

--- build.sh
+++ build.sh
@@ -3,19 +3,8 @@
 #exit if any command fails
 set -e
 
-artifactsFolder="./artifacts"
-
-if [ -d $artifactsFolder ]; then  
-  rm -R $artifactsFolder
-fi
-
 dotnet restore
+dotnet build
 
-# Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
-# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
-
-# Instead, run directly with mono for the full .net version 
-dotnet build -c Release
-
-revision=${TRAVIS_JOB_ID:=1}  
+revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "%04d" $revision) 
